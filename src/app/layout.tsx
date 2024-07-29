f2fefdf367d8577f471eff787694db2f4826a814
yassinkr <kerraiyassin@gmail.com>
2024-07-29 13:14:06 +0100

    first

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "~/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Livedocs",
+  description: "Your go-to collaborative documentation tool",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
